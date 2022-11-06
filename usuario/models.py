4736f0ff437ec013d3e0066b000f05626b00c99b
Henrique César <rickcesaralves07@gmail.com>
2022-11-06 19:58:14 -0300

    Correções de testes de usuários

--- usuario/models.py
+++ usuario/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-  usuario = models.CharField(max_length=40)
+  usuario = models.CharField(max_length=40,unique=True)
   nome = models.CharField(max_length=256)
-  email = models.CharField(max_length=100)
+  email = models.CharField(max_length=100,unique=True)
   data_nascimento = models.DateField()
+  senha = models.CharField(max_length=32, default=False)
 
   def __str__(self) -> str:
     return self.usuario
+
+  def alterar_usuario(self,nome,usuario):
+    self.usuario = usuario
+    self.nome = nome
+
+  # def alterar_senha(self,dados_usuario):
+  #   self.senha = dados_usuario.senha
+
+  
--- usuario/tests.py
+++ usuario/tests.py
@@ -7,17 +7,45 @@ from .views import UsuarioView
 class CriarUsuario(TestCase):
 
   def test_criar_novo_usuario(self):
-    pass
+    self.assertEqual(Usuario.objects.count(),0)
+    usuario = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    usuario.save()
+    self.assertEqual(Usuario.objects.count(),1)
 
   def test_numero_usuarios(self):
-    pass
+    self.assertEqual(Usuario.objects.count(),0)
+    usuario1 = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    usuario1.save()
+    usuario2 = Usuario(usuario='marcialima',nome='Marcia Lima',email='marcia@lima',data_nascimento='2002-04-07', senha='123')
+    usuario2.save()
+    self.assertEqual(Usuario.objects.count(),2)
 
   def test_criar_usuario_ja_existente(self):
-    pass
+    usuario1 = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    usuario1.save()
+    usuario2 = Usuario(usuario='marcialima',nome='Marcia Lima',email='marcia@lima',data_nascimento='2002-04-07', senha='123')
+    usuario2.save()
+    usuario3 = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@a',data_nascimento='2002-04-07',senha='123')
+    with self.assertRaises(Exception) as exception:
+      usuario3.save()
+
+  def test_criar_usuario_email_ja_utilizado(self):
+    usuario1 = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    usuario1.save()
+    usuario2 = Usuario(usuario='marcialima',nome='Marcia Lima',email='marcia@lima',data_nascimento='2002-04-07', senha='123')
+    usuario2.save()
+    usuario3 = Usuario(usuario='joaoa',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    with self.assertRaises(Exception) as exception:
+      usuario3.save()
 
 class AlterarUsuario(TestCase):
   def test_alterar_nome_usuario(self):
-    pass
+    usuario1 = Usuario(usuario='joaosouza',nome='Joao Souza',email='joao@souza',data_nascimento='2002-04-07',senha='123')
+    usuario1.save()
+    user = Usuario.objects.get(usuario='joaosouza')
+    self.assertEqual(user.nome,'Joao Souza')
+    user.alterar_usuario('Joao Sousa', 'joaosouza')
+    self.assertEqual(user.nome,'Joao Sousa')
 
   def test_alterar_nome_usuario_ja_existente(self):
     pass

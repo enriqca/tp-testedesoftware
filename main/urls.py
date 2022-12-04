28bc582deb47b23f67f7698f15615424165517b7
isabellevieirab <ivbisabelle@gmail.com>
2022-12-04 20:54:45 -0300

    primeira versao das outras paginas

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns=[
-  path('', home_page, name='home')
+  path('', home_page, name='home'),
+  path('login', login_page, name='login'),
+  path('dashboard', dashboard_page, name='dashboard'),
+  path('cadastro', cadastro_page, name='cadastro'),
+  path('dashboard/historico', historico_page, name='historico'),
+  path('dashboard/graficos', graficos_page, name='graficos'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render, redirect
 
 def home_page(request):
     return render(request, 'index.html')
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def dashboard_page(request):
+    return render(request, 'dashboard.html')
+
+def cadastro_page(request):
+    return render(request, 'cadastro.html')
+
+def historico_page(request):
+    return render(request, 'historico.html')
+
+def graficos_page(request):
+    return render(request, 'graficos.html')
